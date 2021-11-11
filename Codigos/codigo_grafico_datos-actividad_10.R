69c8223aee4a7900df7ccdf4ebc80f15583c653f
Sebastian Celaya <sebastin.celaya@uc.cl>
2021-11-11 16:53:41 -0300

    Duda Tutoria Ensayo en Grafico

--- Codigos/codigo_grafico_datos-actividad_10.R
+++ Codigos/codigo_grafico_datos-actividad_10.R
@@ -9,18 +9,39 @@ datos = read_excel(file.choose(), sheet = 3, col_names = TRUE, skip = 7)
 
 datos$Mes...2 = factor(datos$Mes...2, levels = c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"))
 
+View(datos)
+# no lo puedo pasar a grafico de lineas  D:
 datos %>%
   filter(Año...1 == 2020) %>%
   ggplot(aes(x = Mes...2, y = Movistar...3))+
+  #geom_line()+
   geom_col(fill = "firebrick4")+
   labs(title = "Conexiones a internet movil 2G en el año 2020",
        subtitle = "En Movistar",
        y = "Conexiones",
        x = "Meses")
 
+datos %>%
+  filter(Año...1 == 2020) %>%
+  ggplot(aes(x = Mes...2, y = Movistar...11))+
+  geom_col(fill = "royalblue4")+
+  labs(title = "Conexiones a internet movil 3G en el año 2020",
+       subtitle = "En Movistar",
+       y = "Conexiones",
+       x = "Meses")+
+  scale_y_continuous(breaks = seq(0,700000, by = 175000))
 
+datos %>%
+  filter(Año...1 == 2020) %>%
+  ggplot(aes(x = Mes...2, y = Movistar...25))+
+  geom_col(fill = "chartreuse4")+
+  labs(title = "Conexiones a internet movil 4G en el año 2020",
+       subtitle = "En Movistar",
+       y = "Conexiones",
+       x = "Meses")+
+  scale_y_continuous(breaks = seq(0, 4000000, by = 1000000))
 
+View(datos)
 
-
-
-
+opciones = c("Cajon", "paseo")
+respuesta_sacamos_a = sample(opciones, 1)
